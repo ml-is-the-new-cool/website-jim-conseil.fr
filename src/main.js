7eafe8a6bad509cb3eefce7a622b27bcff16093b
Hippolyte L. Debernardi <contact@hippolyte-debernardi.com>
2019-08-19 14:06:46 +0200

    idk

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from './App.vue'
 import router from './router'
 import { i18n } from './plugins/i18n';
 import { Trans } from './plugins/Translation'
-import './registerServiceWorker'      // spa support
+import './registerServiceWorker'              // spa support
 import AOS from 'aos'                         // https://michalsnik.github.io/aos/
 import 'aos/dist/aos.css'
-import './assets/styles/core.scss'              // css and js scripts
+import './assets/styles/core.scss'            // css and js scripts
 
-//Vue.use(Bulma);
 Vue.prototype.$i18nRoute = Trans.i18nRoute.bind(Trans);
 Vue.config.productionTip = false;
 
