43a80c878fe59a50940bd71c25bf17686417a90f
Hippolyte L. Debernardi <contact@hippolyte-debernardi.com>
2019-09-13 17:10:59 +0200

    x

---
+++ src/pages/old/post.vue
@@ -0,0 +1,21 @@
+<!-- Post -->
+                <div v-for="(partner, index) in $t('partners.content')"
+                     :key="partner.title"
+                     class="blog-post">
+                  <a :href="partner.link">
+                    <!-- Featured image -->
+                    <div class="featured-image">
+                      <img src="post1.svg" alt="Aix-Marseille Université">
+                    </div>
+                    <!-- Content -->
+                    <div class="content">
+                      <div class="post-title">
+                        {{ partner.name }}
+                      </div>
+                      <p>
+                        {{ partner.description }}
+                      </p>
+                    </div>
+                  </a>
+                </div>
+                <!-- /Post -->
